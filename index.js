9d5ac0088ac30440d1b843262f759a3561c4e183
Maxwell Berkel <maxwell@allbitsnbytes.com>
2015-04-13 16:46:50 -0400

    Added functionality for get(), set() and getHashers() with corresponding tests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
  * Gulp plugin to hash assets and generate asset manifest
  */
 
+var _			= require('lodash');
 var file		= require('vinyl-file');
 var hasher		= require('../asset_hash/');
 var through		= require('through2');
@@ -13,7 +14,121 @@ var util		= require('gulp-util');
   */
  var GulpAssetHasher = function() {
 
- 	return {};
+ 	// Set default configuration
+ 	hasher.set({
+
+ 		// The default algorithm to use to generate hash
+ 		// Options are those available from node's crypto library, getHashes() method
+ 		hasher: 'sha1',
+
+ 		// Length of the hash string generation
+ 		length: 10,
+
+ 		// Whether to replace original file or keep it after hashed file is generated
+ 		replace: false,
+
+ 		// Name to use for the manifest file
+ 		manifest: 'assets.json',
+
+ 		// Template to use for the hashed file
+ 		// Available variables are:  
+ 		// - name		Name of the file
+ 		// - hash		The hash string generated for the file
+ 		// - ext		The extension of the file
+ 		template: '{{name}}-{{hash}}.{{ext}}'
+ 	});
+
+
+ 	/**
+ 	 * Set configuration
+ 	 *
+ 	 * @param {object} options Config options to add or update
+ 	 */
+ 	var set = function(options) {
+ 		hasher.set(options);
+ 	};
+
+
+ 	/**
+ 	 * Get configuration options or value for specified key
+ 	 *
+ 	 * @param {*} key The key to find values for.  If key is empty or not present in config an empty string will be returned.  If no key is specified/undefined the whole config object will be returned.
+ 	 * @return {*} Config value for specified key or whole config object
+ 	 */
+ 	var get = function(key) {
+ 		return typeof key === 'undefined' ? hasher.get() : hasher.get(key);
+ 	};
+
+
+ 	/**
+ 	 * Get a list of supported hash algorithms
+ 	 *
+ 	 * @return {array} Array of hashes
+ 	 */
+ 	var getHashers = function() {
+ 		return hasher.getHashers();
+ 	}
+
+
+ 	/**
+ 	 * Hash files from provided stream
+ 	 *
+ 	 * @param {object} options The options to customize the hash
+ 	 * @return {object} Returns stream (through object)
+ 	 */
+ 	var hash = function(options) {
+ 		options = _.isObject(options) ? options : {};
+
+ 		return through.obj(function(file, enc, cb) {
+
+ 			// If file is null continue
+ 			if (file.isNull()) {
+ 				cb(null, file);
+
+ 				return;
+ 			}
+
+ 			// If stream throw an error
+ 			if (file.isStream()) {
+ 				cb(new util.PluginError('gulp-asset-hash', 'Sorry, streaming is not supported.'));
+
+ 				return;
+ 			}
+
+ 			// Hash file
+ 			try {
+ 				var result = hasher.hashFiles(file.path, options);
+ 			}
+ 			catch(e) {
+ 				cb(util.PluginError('gulp-asset-hash', e));
+
+ 				return;
+ 			}
+
+ 			// Pass updated file and continue
+ 			cb(null, file);
+ 		});
+ 	};
+
+
+ 	/**
+ 	 * Save manifest file
+ 	 *
+ 	 * @param {object} options The options to configure manifest file
+ 	 * @return {object} Returns stream (through object)
+ 	 */
+ 	var saveManifest = function(options) {
+
+ 	};
+
+
+ 	return {
+ 		get: get,
+ 		set: set,
+ 		hash: hash,
+ 		saveManifest: saveManifest,
+ 		getHashers: getHashers
+ 	};
  };
 
 
--- test/index.js
+++ test/index.js
@@ -6,7 +6,7 @@ var _			= require('lodash');
 var expect		= require('chai').expect;
 var fs			= require('fs');
 var glob		= require('glob');
-var hasher		= require('../asset_hash/');
+var hasher		= require('../');
 var path		= require('path');
 
 
@@ -17,13 +17,214 @@ var tmpDir		= './tmp/';
 
 // Utility Functions
 
-// TODO:  Add utility functions as needed
+/**
+ * Clean up  test environment
+ * @param {string} path The path to directory to remove
+ */
+function removeTestDir(path) {
+	var files = [];
+
+	if(fs.lstatSync(path).isDirectory()) {
+		files = fs.readdirSync(path);
+
+		files.forEach(function(file, index) {
+			var curPath = path + "/" + file;
+
+			if(fs.lstatSync(curPath).isDirectory()) { 
+				removeTestDir(curPath);
+			} else {
+				fs.unlinkSync(curPath);
+			}
+		});
+
+		fs.rmdirSync(path);
+    }
+}
+
+/**
+ * Add test files
+ * @param {array} files Test files to create
+ */
+function addTestFiles(files) {
+	if (!_.isArray(files)) {
+		files = [files];
+	}
+
+	// Loop and add files
+	files.forEach(function(file, index) {
+		var filePath 	= path.dirname(file).split('/');
+		var curDir 		= '';
+
+		// Create parent directories for file if necessary
+		while (filePath.length > 0) {
+			curDir += filePath.shift();
+
+			try {
+				fs.lstatSync(curDir);
+			} 
+			catch(e) {
+				fs.mkdirSync(curDir);
+			}
+			
+			curDir += '/';
+		}	
+
+		fs.writeFileSync(file, 'test file '+index);
+	});
+}
+
+/**
+ * Remove test files
+ * @param  {array} files The files to remove
+ */
+function removeTestFiles(files) {
+	if (!_.isArray(files)) {
+		files = [files];
+	}
+
+	files.forEach(function(file, index) {
+		fs.unlinkSync(file);
+	});
+}
 
 
-description('Check if Gulp Asset Hash is defined', function() {
+describe('Test if Gulp Asset Hash is defined', function() {
 
 	it('Should be an object', function() {
-		expect(hasher).to.exist.and.be.an.object('object');
+		expect(hasher).to.exist.and.be.an('object');
 	})
-	
+
+})
+
+
+describe('Test if methods exist', function() {
+
+	var methods = ['get', 'set', 'hash', 'saveManifest'];
+
+	methods.forEach(function(method) {
+		it('Should have method: ' + method, function() {
+			expect(hasher[method]).to.be.a('function');
+		})
+	});
+
+})
+
+
+describe('Test config functionality', function() {
+
+	it('Should get all config', function() {
+		var config = hasher.get();
+
+		expect(config).to.be.an('object');
+	})
+
+	it('Should have default config values', function() {
+		var defaults = ['hasher', 'length', 'manifest', 'replace', 'template'];
+		var config = hasher.get();
+
+		defaults.forEach(function(property) {
+			expect(config).to.have.ownProperty(property);
+		})
+	})
+
+	it('Should get config value for specified key', function() {
+		var value = hasher.get('hasher');
+
+		expect(value).to.not.be.empty;
+	})
+
+	it('Should return empty for invalid key', function() {
+		var value = hasher.get('an-invalid-key-aubpasdyp12y3hkashdf');
+
+		expect(value).to.be.empty;
+	})
+
+	it('Should set config value', function() {
+		hasher.set({testkey1: 'testvalue1'});
+		hasher.set({testkey2: 'testvalue2'});
+
+		expect(hasher.get('testkey1')).to.equal('testvalue1');
+		expect(hasher.get('testkey2')).to.equal('testvalue2');
+	})
+
+})
+
+
+describe('Test default config values', function() {
+
+	it('Should have a valid hasher', function() {
+		var hashers = hasher.getHashers();
+		var myHasher = hasher.get('hasher');
+
+		expect(myHasher).to.be.a('string');
+		expect(hashers).to.be.an('array').and.contains(myHasher);
+	})
+
+	it('Should have a valid length', function() {
+		expect(hasher.get('length')).to.be.a('number').and.be.at.least(10);
+	})
+
+	it('Should have a manifest file', function() {
+		expect(hasher.get('manifest')).to.match(/[a-zA-Z-0-9_\/-]+\.json/);
+	})
+
+	it('Should have a boolean replace value', function() {
+		expect(hasher.get('replace')).to.be.a('boolean');
+	})
+
+	it('Should have a hashed filename template', function() {
+		expect(hasher.get('template')).to.not.be.empty;
+	})
+
+	it.skip('Should have a valid template format', function() {
+		hasher.set('{{name}}-{{hash}}.{{ext}}');
+
+	})
+
+})
+
+
+describe('Test hashing functionality', function() {
+
+	beforeEach(function() {
+		addTestFiles(testFiles);
+	})
+
+	afterEach(function() {
+		removeTestDir(tmpDir);
+	})
+
+	it.skip('Should hash a single file', function() {
+
+	})
+
+	it.skip('Should hash multiple files', function() {
+
+	})
+
+	it.skip('Should hash a file twice and remove the previous hashed file', function() {
+
+	})
+
+})
+
+
+describe('Test manifest file', function() {
+
+	beforeEach(function() {
+		addTestFiles(testFiles);
+	})
+
+	afterEach(function() {
+		removeTestDir(tmpDir);
+	})
+
+	it.skip('Should create a manifest file', function() {
+
+	})
+
+	it.skip('Should have all hashed files in manifest file', function() {
+
+	})
+
 })
